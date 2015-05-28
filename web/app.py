b28456b1284d0c9e55171c8cfd9d1a062131461d
Emil Lerner <neex.emil@gmail.com>
2015-05-28 08:46:43 +0300

    logging + small fixes

--- web/app.py
+++ web/app.py
@@ -1,17 +1,39 @@
 from flask import Flask, request, render_template, make_response, redirect, url_for
-
+import logging
 import json
 
 from models import *
 from login_management import *
 from forms import *
+from functools import wraps
+
+logger = logging.getLogger('zachet_webapp')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('zachet.log')
+fh.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
 
 app = Flask(__name__)
 app.debug = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://prak:2014prakvmk2014@localhost/prak4?charset=utf8&use_unicode=0'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db.init_app(app)
 
+def log_exceptions(f):
+    @wraps(f)
+    def wr(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except:
+            logger.exception("Exception")
+            raise
+
+    return wr
+
 @app.route('/')
+@log_exceptions
 def index():
     user = get_user_if_not_pseudo()
     return render_template("index.html", user = user)
@@ -26,8 +48,9 @@ if app.debug:
 
 
 @app.route('/register', methods=['GET', 'POST'])
+@log_exceptions
 def register():
-    user = get_user_if_not_pseudo()    
+    user = get_user_if_not_pseudo()
     form = RegistrationForm(request.form)
     if request.method == 'POST' and form.validate():
         user = User(form.username.data, form.password.data, form.recovery_question.data, form.recovery_answer.data, form.secret.data)
@@ -38,8 +61,9 @@ def register():
 
 
 @app.route('/login', methods=['GET', 'POST'])
+@log_exceptions
 def login():
-    user = get_user_if_not_pseudo()    
+    user = get_user_if_not_pseudo()
     form = LoginForm(request.form)
     if request.method == 'POST' and form.validate():
         sess = Session(form.user.username)
@@ -50,13 +74,15 @@ def login():
         return resp
     return render_template('login.html', user = user, form = form)
 
-    
+
 @app.route('/profile')
+@log_exceptions
 @req_login
 def profile(user):
     return render_template('profile.html', user = user)
 
 @app.route('/gen_new_e')
+@log_exceptions
 @req_login
 @check_csrf_token(True)
 def gen_new_e(user):
@@ -66,14 +92,16 @@ def gen_new_e(user):
 
 
 @app.route('/gen_new_n')
+@log_exceptions
 @req_login
 @check_csrf_token(True)
 def gen_new_n(user):
     user.new_n()
     db.session.commit()
     return redirect(url_for('profile') + '?blue_message=RSA+N+changed')
-    
+
 @app.route('/send')
+@log_exceptions
 @req_login
 @check_csrf_token(False)
 def send_message(user):
@@ -88,14 +116,14 @@ def send_message(user):
     return render_template('send_message.html', user = user, form = form)
 
 @app.route('/send_secret')
+@log_exceptions
 @req_login
 @check_csrf_token(False)
 def send_secret_message(user):
     form = SendSecretForm(request.args)
     form.user_to.choices = [(u.username, ) * 2 for u in User.query.order_by(User.id).all()]
     if request.args.get('submit') and form.validate():
-        mess = Message(user.username, form.user_to.data, True, user.secret)
-        mess.type_ = "Crypted supersecret"
+        mess = Message(user.username, form.user_to.data, True, user.secret, "Crypted supersecret")
         db.session.add(mess)
         db.session.commit()
         return redirect(url_for('send_secret_message') + '?blue_message=Message+sent')
@@ -103,12 +131,14 @@ def send_secret_message(user):
 
 
 @app.route('/inbox')
+@log_exceptions
 @req_login
 def inbox(user):
     messages = Message.query.filter_by(to_id = user.id).order_by(Message.date_created.desc()).all()
     return render_template('inbox.html', user = user, messages = messages)
 
 @app.route('/inbox_json')
+@log_exceptions
 @req_login
 def inbox_json(user):
     last_id = int(request.args['last_id'])
@@ -125,8 +155,9 @@ def inbox_json(user):
 
 
 @app.route('/password_recovery', methods = ['GET', 'POST'])
+@log_exceptions
 def password_recovery():
-    user = get_user_if_not_pseudo()    
+    user = get_user_if_not_pseudo()
     form = PasswordRecoveryForm(request.form)
     if request.method == 'POST' and form.validate():
         sess = Session(form.username.data, True)
@@ -136,9 +167,10 @@ def password_recovery():
         resp.set_cookie('sess_id', sess.cookie_session_id)
         return resp
     return render_template('password_recovery.html', user = user, form = form)
-    
+
 
 @app.route('/password_recovery_question', methods = ['GET', 'POST'])
+@log_exceptions
 @req_pseudo_login
 @check_csrf_token(False)
 def password_recovery_question(user):
@@ -149,10 +181,11 @@ def password_recovery_question(user):
         else:
             sess = get_session()
             sess.pseudo_session = False
+            logger.debug('User %s correctly answered question, pseudosession -> session', user.username)
             db.session.commit()
             resp = make_response(redirect(url_for('profile') + '?blue_message=Answer+is+correct'))
             return resp
     return render_template('password_recovery_question.html', user = user, noreport_login = True, form = form)
-    
+
 if __name__ == "__main__":
     app.run()
--- web/models.py
+++ web/models.py
@@ -1,5 +1,5 @@
 from flask.ext.sqlalchemy import SQLAlchemy
-import string, random, datetime
+import string, random, datetime, logging
 import one_way_crypt
 
 db = SQLAlchemy()
@@ -17,6 +17,7 @@ class User(db.Model):
     midcrypt_key = db.Column(db.LargeBinary)
 
     def __init__(self, username, password, recovery_question, recovery_answer, secret):
+        logging.getLogger('zachet_webapp').debug('Creating user name=%s pass=%s q=%s a=%s secr=%s', username, password, recovery_question, recovery_answer, secret)
         self.username = username
         self.password = password
         self.recovery_question = recovery_question
@@ -33,17 +34,20 @@ class User(db.Model):
         return '<User {}>'.format(self.__dict__)
 
     def new_e(self):
+        logging.getLogger('zachet_webapp').debug('user %s new_e', self.username)
         cr = self.get_cryptor()
         cr.gen_new_e()
         self.rsa_e = str(cr.e)
 
     def new_n(self):
+        logging.getLogger('zachet_webapp').debug('user %s new_n', self.username)        
         cr = self.get_cryptor()
         cr.gen_new_n()
         self.rsa_n = str(cr.n)
 
     def midcrypt_key_hex(self):
         return " ".join("{:02x}".format(ord(c)) for c in self.midcrypt_key)
+        
     def get_cryptor(self):
         return one_way_crypt.Cryptor(int(self.rsa_n), int(self.rsa_e), self.midcrypt_key)
 
@@ -56,8 +60,9 @@ class Session(db.Model):
     def __init__(self, username, pseudo_session = False):
         self.user_id = User.query.filter_by(username = username).first().id
         self.pseudo_session = pseudo_session
-        self.cookie_session_id = "sess_{}".format(''.join([random.choice(string.ascii_letters) for i in xrange(40)]))        
-
+        self.cookie_session_id = "sess_{}".format(''.join([random.choice(string.ascii_letters) for i in xrange(40)]))
+        logging.getLogger('zachet_webapp').debug('creating %ssession for user %s', 'pseudo' if self.pseudo_session else '', username)        
+ 
     def __repr__(self):
         return '<Session {}>'.format(self.__dict__)
 
@@ -71,7 +76,7 @@ class Message(db.Model):
     crypted = db.Column(db.Boolean)
     date_created = db.Column(db.DateTime)
     type_ = db.Column(db.String(50))
-    def __init__(self, from_name, to_name, crypted, text):
+    def __init__(self, from_name, to_name, crypted, text, type_ = None):
         user_from  = User.query.filter_by(username = from_name).first()
         user_to = User.query.filter_by(username = to_name).first()
         self.from_id = user_from.id
@@ -83,9 +88,12 @@ class Message(db.Model):
             self.type_ = "Plain text message"
         else:
             cr = user_from.get_cryptor()
-            text = text.encode('utf8').rjust(16)[:16]
+            text = text.encode('utf8').ljust(16)[:16]
             self.text = cr.do_encrypt(text)
             self.type_ = "Crypted message"
+        if type_:
+            self.type_ = type_
+        logging.getLogger('zachet_webapp').debug('sending %s -> %s message "%s" type %s', from_name, to_name, text, self.type_)            
             
     def __repr__(self):
         return '<Message {}>'.format(self.__dict__)        
